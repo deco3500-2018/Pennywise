d2b0c44c6cdc36cb07f11557cb60a8e1920cffea
seamas zhou <seamaszhou@gmail.com>
2018-08-28 08:57:00 +1000

    added style and more for Home

--- app/components/Home/index.js
+++ app/components/Home/index.js
@@ -1,17 +1,52 @@
 import React ,{Component} from 'react';
-import {View, TextInput, Text } from 'react-native';
+import {View, TextInput, Text, Button, Alert } from 'react-native';
+import styles from './style';
+
+
 
 class Home extends Component{
+
+  state = {username:"", password:""}
+
+  static navigationOptions = {
+    header: null
+  }
+  checkLogin(){
+    const {username, password} = this.state
+    // if(username == 'admin' && password == 'admin'){
+      fetch('http://127.0.0.1:8081', {method:'POST',body: JSON.stringify({username,password})})
+      .then(res =>{
+        return res.text();
+      })
+      .then(res => {
+        if (res === '1'){
+          this.props.navigation.navigate('dashboard')
+        }else{
+          Alert.alert('Error', 'Username or Password wrong', [{text:'Okay'}])
+        }
+
+    })
+  }
+    // } else {
+    //   Alert.alert('Error', 'Username or Password wrong', [{text:'Okay'}]);
+    // }
+
+
   render () {
+    const {heading, input, parent} = styles
+
     return (
-      <View>
-        <Text>Login into the application</Text>
-        <TextInput/>
-        <TextInput/>
+      <View style={parent}>
+        <Text style = {heading}>Login into the application</Text>
+        <TextInput style = {input} placeholder="Username" onChangeText={text => this.setState({username: text})}/>
+        <TextInput style = {input} placeholder="Password" secureTextEntry={true} onChangeText={text=>this.setState({password:text})}/>
+        <Button title = {"Login"} onPress={_ => this.checkLogin()}/>
       </View>
 
     )
   }
+
 }
+
 
 export default Home
