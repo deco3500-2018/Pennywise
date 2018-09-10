3c2341cf663a8c29351e9ea8abe0dc9966821e70
seamas zhou <seamaszhou@gmail.com>
2018-09-10 16:51:36 +1000

    removed unnecessary files

--- Backup/App.js
+++
@@ -1,8 +0,0 @@
-import React, {Component} from 'react';
-import {createStackNavigator} from 'react-navigation';
-import Home from './app/components/Home';
-import Dashboard from './app/components/Dashboard';
-export default createStackNavigator({
-  home: Home,
-  dashboard: Dashboard
-})
--- Backup/server/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$username = $_POST['username'];
-$password = $_POST['password'];
-
-if($username == 'admin' && $password == 'admin'){
-echo 1;}
-else{echo 0;}
-
-?>
