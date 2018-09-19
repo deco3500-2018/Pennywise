6210d3d08dac7828e637e85bbd95c1eb2f5aee63
seamas zhou <seamaszhou@gmail.com>
2018-09-19 15:25:34 +1000

    clean the path and change readme

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,10 +1,10 @@
-function show() {
-  new Notification('Hi', {
-    icon: 'assets/images/logo.png',
-    body: 'Are you sure you want to spend 100$?'
-  });
-  chrome.tabs.create({url:"http://google.com/"});
-}
+// function show() {
+//   new Notification('Hi', {
+//     icon: 'assets/images/logo.png',
+//     body: 'Are you sure you want to spend 100$?'
+//   });
+//   chrome.tabs.create({url:"http://google.com/"});
+// }
 
 function url(){
   chrome.tabs.getSelected(null, function(tab){
@@ -23,23 +23,23 @@ if (!localStorage.isInitialized) {
 
 }
 
-if (window.Notification) {
-  if(JSON.parse(localStorage.isActivated)){show();}
-  let interval = 0;
-
-  setInterval(function() {
-    interval++;
-
-    if (
-      JSON.parse(localStorage.isActivated) &&
-        localStorage.frequency <= interval
-    ) {
-      show();
-      interval = 0;
-    }
-  }, 60000);
-
-};
+// if (window.Notification) {
+//   if(JSON.parse(localStorage.isActivated)){show();}
+//   let interval = 0;
+//
+//   setInterval(function() {
+//     interval++;
+//
+//     if (
+//       JSON.parse(localStorage.isActivated) &&
+//         localStorage.frequency <= interval
+//     ) {
+//       show();
+//       interval = 0;
+//     }
+//   }, 60000);
+//
+// };
 
 $(document).ready(function(){
   url();
