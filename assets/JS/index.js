db53657ce6900d9d4b6cdf79497d73441ed376df
seamas zhou <seamaszhou@gmail.com>
2018-09-26 20:29:36 +1000

    optimised detect function

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,46 +1,39 @@
-// function show() {
-//   new Notification('Hi', {
-//     icon: 'assets/images/logo.png',
-//     body: 'Are you sure you want to spend 100$?'
-//   });
-//   chrome.tabs.create({url:"http://google.com/"});
-// }
+function show() {
+  new Notification('Hi', {
+    icon: 'assets/images/logo.png',
+    body: 'Are you sure you want to spend 100$?'
+  });
+  chrome.tabs.create({url:"http://google.com/"});
+}
 
 function url(){
   chrome.tabs.getSelected(null, function(tab){
     var link = document.createElement('a');
     link.href = tab.url;
-    $('#test').html(link.hostname);
-    console.log(link.href);
-    console.log(link.hostname);
-  })
+
+    if (link.href.includes("cart")) {
+        $('#detect').html(link.href);
+        show();
+    }
+
+  });
 }
 
-if (!localStorage.isInitialized) {
-  localStorage.isActivated = true;
-  localStorage.frequency = 1;
-  localStorage.isInitialized = true;
+chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+  if (changeInfo.status == 'complete' && tab.active) {
+    console.log(tab);
 
-}
+    chrome.tabs.getSelected(null, function(tab){
+      var link = document.createElement('a');
+      link.href = tab.url;
 
-// if (window.Notification) {
-//   if(JSON.parse(localStorage.isActivated)){show();}
-//   let interval = 0;
-//
-//   setInterval(function() {
-//     interval++;
-//
-//     if (
-//       JSON.parse(localStorage.isActivated) &&
-//         localStorage.frequency <= interval
-//     ) {
-//       show();
-//       interval = 0;
-//     }
-//   }, 60000);
-//
-// };
+      if (link.href.includes("cart")) {
+        chrome.tabs.remove(tab.id,function(){})
+          // $('#detect').html(link.href);
+          show();
+      }
 
-$(document).ready(function(){
-  url();
+    });
+
+  }
 })
