77c58bb466bb7c7c3864a7e4f9269152c593afc9
seamas zhou <seamaszhou@gmail.com>
2018-09-30 15:16:31 +1000

    optimise the code

--- Plugin/index.js
+++ Plugin/index.js
@@ -21,30 +21,34 @@
 
 function show() {
   chrome.tabs.getSelected(null, function(tab) {
-    chrome.tabs.sendMessage(tab.id, {method: "getPrice"}, function(response) {
-        if(response.method=="getPrice"){
-            alltext = response.data;
-            console.log(alltext);
-            new Notification('Hi', {
-              icon: 'assets/images/logo.png',
-              body: 'Price detect:'+ alltext.replace(/\s+/g, '') + '?'
-            });
-            chrome.tabs.create({url:"https://seamaszhou.wixsite.com/puzzlepay"});
-        }
+    chrome.tabs.sendMessage(tab.id, {
+      method: "getPrice"
+    }, function(response) {
+      if (response.method == "getPrice") {
+        alltext = response.data;
+        console.log(alltext);
+        new Notification("Hi", {
+          icon: "assets/images/logo.png",
+          body: "Price detect:" + alltext.replace(/\s+/g, "") + "?"
+        });
+        chrome.tabs.create({
+          url: "https://seamaszhou.wixsite.com/puzzlepay"
+        });
+      }
     });
-});
+  });
 
 }
 
 // This function is using for detect URL address
-function url(){
+function url() {
   chrome.tabs.getSelected(null, function(tab) {
-    var link = document.createElement('a');
+    var link = document.createElement("a");
     link.href = tab.url;
 
     if (link.href.includes("cart")) {
-        $('#detect').html(link.href);
-        show();
+      $("#detect").html(link.href);
+      show();
     }
 
   });
@@ -52,20 +56,20 @@ function url(){
 
 
 // Detect if user is in the shopping cart page
-chrome.tabs.onUpdated.addListener( function(tabId, changeInfo, tab) {
-  if (changeInfo.status == 'complete' && tab.active) {
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  if (changeInfo.status === "complete" && tab.active) {
     console.log(tab);
 
     chrome.tabs.getSelected(null, function(tab) {
-      var link = document.createElement('a');
+      var link = document.createElement("a");
       link.href = tab.url;
 
       // The close function is working for Amazon/Ebay/Taobao
-      if (link.href.includes("cart") ) {
+      if (link.href.includes("cart")) {
         //close tab
-        chrome.tabs.remove(tab.id,function(){});
-          // $('#detect').html(link.href);
-          show();
+        chrome.tabs.remove(tab.id, function() {});
+        // $('#detect').html(link.href);
+        show();
       }
 
     });
