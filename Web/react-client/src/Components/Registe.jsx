147920a70c041d8f34bb9a5057952ec7e1630710
seamas zhou <seamaszhou@gmail.com>
2018-10-02 11:01:57 +1000

    comment

--- Web/react-client/src/Components/Registe.jsx
+++ Web/react-client/src/Components/Registe.jsx
@@ -21,6 +21,7 @@ class Registe extends Component {
     document.getElementById('googleButton')
     return (<div>
 
+      // Need get a clientId 
       <div id="googleButton">
 
         <GoogleLogin clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com" buttonText="Login with Google" onSuccess={responseGoogle} onFailure={responseGoogle}/>
